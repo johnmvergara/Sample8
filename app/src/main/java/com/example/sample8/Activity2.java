d6e59c30e783e8a111eedd56989dfa9cfc07864d
johnmvergara <johnmichael.vergara.iics@ust.edu.ph>
2019-07-12 13:11:50 +0800

    Initial commit

--- app/src/main/java/com/example/sample8/Activity2.java
+++ app/src/main/java/com/example/sample8/Activity2.java
@@ -7,6 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -20,14 +26,69 @@ public class Activity2 extends AppCompatActivity {
         txtPassword = findViewById(R.id.tvPassword);
     }
 
-    public void displayUser(View v){
+    public void displayUser(View v) {
         SharedPreferences sp = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
         txtName.setText(sp.getString("user", ""));
         txtPassword.setText(sp.getString("pwd", ""));
     }
 
-    public void goPrevious(View v){
+    public void goPrevious(View v) {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
+    }
+
+    /* Para madisplay yung nakasave sa internal na makikita sa user.txt using toast */
+    public void displayInternal(View v) {
+        FileInputStream fis = null;
+        StringBuffer buffer = new StringBuffer();
+        int letter = 0;
+        try {
+            fis = openFileInput("user.txt");
+            while ((letter = fis.read()) != -1) {
+                buffer.append((char) letter);
+            }
+            Toast.makeText(this, buffer.toString(), Toast.LENGTH_LONG).show();
+            /* firstLetter = "" + (char)fis.read();*/
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        txtName.setText(buffer.substring(0, buffer.indexOf(",")));
+        txtPassword.setText(buffer.substring(buffer.indexOf(",")+1));
+    }
+
+    /* Para madisplay yung nakasave sa external na makikita sa sdcard na user2.text using toast */
+    public void displayExternal(View v) {
+        FileInputStream fis = null;
+        StringBuffer buffer = new StringBuffer();
+        int letter = 0;
+        try {
+            File file = new File(getExternalFilesDir(null), "user2.text"); //matic na siya na hahanap ng pat
+            fis = new FileInputStream(file);
+            while ((letter = fis.read()) != -1) {
+                buffer.append((char) letter);
+            }
+            Toast.makeText(this, buffer.toString(), Toast.LENGTH_LONG).show();
+            /* firstLetter = "" + (char)fis.read();*/
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        txtName.setText(buffer.substring(0, buffer.indexOf(",")));
+        txtPassword.setText(buffer.substring(buffer.indexOf(",")+1));
     }
 }
--- app/src/main/java/com/example/sample8/MainActivity.java
+++ app/src/main/java/com/example/sample8/MainActivity.java
@@ -3,15 +3,22 @@ package com.example.sample8;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtName, txtPassword;
+    EditText txtName, txtPassword, txtEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +40,97 @@ public class MainActivity extends AppCompatActivity {
         editor.putString("pwd", txtPassword.getText().toString());
         editor.commit();
         Toast.makeText(this, "saved in userInfo.xml1", Toast.LENGTH_LONG).show();
+    }
+
+    public void saveInternal(View v){
+        FileOutputStream fos=null;
+        try {
+            fos = openFileOutput("user.txt", Context.MODE_PRIVATE);
+            fos.write((txtName.getText().toString() + ",").getBytes());
+            fos.write(txtPassword.getText().toString().getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Toast.makeText(this, "data saved in Internal storage!", Toast.LENGTH_LONG).show();
+
+    }
+
+//sa sd card nakasave to
+    public void saveExternal(View v){
+        FileOutputStream fos = null;
+        File file = new File(getExternalFilesDir(null),"user2.text");
+        try {
+            fos = new FileOutputStream(file);
+            fos.write((txtName.getText().toString() + ",").getBytes());
+            fos.write(txtPassword.getText().toString().getBytes());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+        Toast.makeText(this, "saved data in external storage...", Toast.LENGTH_LONG).show();
+    }
+/* //di pa gumana kaya comment muna
+    public void savePublic(View v){
+        FileOutputStream fos = null;
+        File file = new File(getExternalFilesDir() + "/Documents/user4.txt");
+        try {
+            fos = new FileOutputStream(file);
+            fos.write((txtName.getText().toString() + ",").getBytes());
+            fos.write(txtPassword.getText().toString().getBytes());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+        Toast.makeText(this, "saved data in external storage...", Toast.LENGTH_LONG).show();
+    }
+    */
+//para malocate yung cache, data>data>project name>cache pero di yata to naguupdate
+public void saveCache(View v){
+    FileOutputStream fos = null;
+    File file = new File(getExternalCacheDir(), "user4.txt");
+    try {
+        fos = new FileOutputStream(file);
+        fos.write((txtName.getText().toString() + ",").getBytes());
+        fos.write(txtPassword.getText().toString().getBytes());
+
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    } catch (IOException e){
+        e.printStackTrace();
+    } finally {
+        try {
+            fos.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    Toast.makeText(this, "saved data in cache storage...", Toast.LENGTH_LONG).show();
     }
 }
